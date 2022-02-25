93cc0fffa6101b712ff37417c444e2c4cce3f02c
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-02-25 10:47:15 +0100

    missing last optional task

---
+++ src/no/hvl/dat110/messages/PublishMsg.java
@@ -0,0 +1,41 @@
+package no.hvl.dat110.messages;
+
+import no.hvl.dat110.common.TODO;
+
+public class PublishMsg extends Message {
+	
+	// message sent from client to create publish a message on a topic 
+
+	// TODO:
+	// Implement object variables - a topic and a message is required
+
+	// Constructor, get/set-methods, and toString method
+	// as described in the project text
+	
+	private String message;
+	private String topic;
+
+	public PublishMsg (String user, String topic, String message) {
+		super(MessageType.PUBLISH, user);
+		this.topic = topic;
+		this.message = message;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "PublishMsg [message=" + message + ", topic=" + topic + "]";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
